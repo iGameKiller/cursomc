5a37f1c00c40b36fd4d4cc16518a98bfd113471b
Pedro <joaopedromend14@gmail.com>
2021-03-24 15:28:46 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/avante/cursomc/CursomcApplication.java
+++ src/main/java/com/avante/cursomc/CursomcApplication.java
@@ -1,9 +1,12 @@
 package com.avante.cursomc;
 
+import com.avante.cursomc.CategoriaResources.CategoriaResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = CategoriaResource.class)
 public class CursomcApplication {
 
     public static void main(String[] args) {
